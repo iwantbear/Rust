6c13ad2be24e2f6cda6ad5bd6e25513f85ee5f66
황규한 <gyuhanhwang@gmail.com>
2026-07-27 19:53:12 +0900

    feat: add mut in functions

--- basics/8_func_codeblocks.rs
+++ basics/8_func_codeblocks.rs
@@ -41,3 +41,78 @@ fn main() {
     let my_number = print_number (7, 8);
     println!("{}", my_number);
 }
+
+//------------------------------------------------------------------
+// Ownership을 function에 접목
+// 자세한 Ownership은 다음장
+// 밑의 코드는 아무 문제 없음
+
+fn print_country(country_name: String) {
+    println!("My country is {country_name}");
+}
+
+fn main() {
+    let country = "대한민국".to_string();
+    print_country(country);
+}
+
+
+
+// 위 코드에서 print_country(country); 한줄만 추가하면 오류가남
+// 밑의 경우를 move semantics 라고함
+// 첫번째 print_country(country); 에서 "대한민국" 이라는 데이터는 country가 소유
+// 근데 이걸 print_country로 넘겨줌으로써 소유권이 country_name으로 변경됨
+// 그리고 함수가 끝나기에 contry_name 자체도 사라지고 데이터(대한민국)도 사라짐
+// print_country(&country); 로 쓰면 빌려주는 것이기에 오류 안남
+
+fn print_country(country_name: String) {
+    println!("My country is {country_name}");
+}
+
+fn main() {
+    let country = "대한민국".to_string();
+    print_country(country);
+    print_country(country);
+}
+
+
+// mut를 function에 접목
+// 자세한 mut 내용은 다음장에
+
+// main 함수에서 수정가능한 문자열 타입인 my_country를 정의함 (캐나다로)
+// &mut를 붙여서 수정가능하게 add_is_great 함수로 넘김
+// my_country 라는 변수 이름을 자연스럽게 country_name으로 바꿈
+// rust는 넘길때, 호출할때 전부 변경할 수 있는 기준 쌍을 맞춰야 함
+// 즉, 넘길때 &mut 였으면 호출할때도 &mut를 명시해야함 
+// country_name이 캐나다인데 뒤에 is great!를 붙임
+// 다시 main으로 돌아가서 add_is_great 한번더 호출
+
+// 참고로 .push_str 같은 매서드를 사용하면 *(역참조)를 알아서 수행해줌
+
+fn add_is_great(country_name: &mut String) {
+    country_name.push_str(" is great!");
+    println!("Now it says: {country_name}");
+}
+
+fn main() {
+    let mut my_country = "캐나다".to_string();
+    add_is_great(&mut my_country);
+    add_is_great(&mut my_country);
+}
+
+
+
+// 위 코드랑 다른 유일한 차이점은 이 코드에선 소유권을 아예 넘겨버림
+// main 함수 안에선 my_country가 소유권을 갖고 있는데 
+// 두번째 줄의 add_is_great(my_country); 이 부분에서 (my_country) 앞에
+// &나 &mut가 없기 때문에 아예 소유권을 넘겨버린것
+
+fn add_is_great(mut country_name: String) {
+    country_name.push_str(" is great!");
+    println!("Now it says: {country_name}");
+}
+
+fn main() {
+    let mut my_country = "대한민국".to_string();
+    add_is_great(my_country);
+}
